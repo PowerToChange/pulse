dd7b97d34449c4975c2a93f7a19fb66cba2ad5db
Andrew Roth <andrewroth@gmail.com>
2013-09-13 16:32:38 -0400

    am getting closer to projects totals pulled from pat
    need to use yaml insead of json to keep the ordered hash and symbols

--- lib/pat.rb
+++ lib/pat.rb
@@ -1,16 +1,60 @@
 module Pat
   def project_acceptance_totals(campus_ids, secondary_sort = {})
     params = { :campus_ids => campus_ids, :secondary_sort => secondary_sort }
-    res = Net::HTTP.get_response("pat.powertochange.org", "/projects/campus_project_acceptance_totals?#{params.to_query}", 80)
-    return JSON.parse(res.body)
+    if Rails.env.development?
+      puts "/projects/campus_project_acceptance_totals?#{params.to_query}"
+      res = Net::HTTP.get_response("localhost", "/projects/campus_project_acceptance_totals?#{params.to_query}", 3001)
+    else
+      puts "/projects/campus_project_acceptance_totals?#{params.to_query}"
+      url = URI.parse("https://pat.powertochange.org/projects/campus_project_acceptance_totals?#{params.to_query}")
+      http = Net::HTTP.new url.host, url.port
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      http.use_ssl = true
+      res = nil
+      http.start do |agent|
+        puts "#{url.path}?#{url.query}"
+        res = agent.get("#{url.path}?#{url.query}")
+      end
+    end
+
+    return YAML::load(res.body)
   end
 
   def project_applying_totals(campus_ids)
     params = { :campus_ids => campus_ids }
-    res = Net::HTTP.get_response("pat.powertochange.org", "/projects/campus_project_applying_totals?#{params.to_query}", 80)
-    return JSON.parse(res.body)
+    if Rails.env.development?
+      res = Net::HTTP.get_response("localhost", "/projects/campus_project_applying_totals?#{params.to_query}", 3001)
+    else
+      url = URI.parse("https://pat.powertochange.org/projects/campus_project_applying_totals?#{params.to_query}")
+      http = Net::HTTP.new url.host, url.port
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      http.use_ssl = true
+      res = nil
+      http.start do |agent|
+        puts url.path
+        res = agent.get("#{url.path}?#{url.query}")
+      end
+    end
+
+    return YAML::load(res.body)
   end
 
-  def projects_count_hash
+  def projects_count_hash(project_ids = nil)
+    params = project_ids.present? ? { :project_ids => project_ids } : {}
+    if Rails.env.development?
+      res = Net::HTTP.get_response("localhost", "/projects/projects_count_hash?#{params.to_query}", 3001)
+    else
+      url = URI.parse("https://pat.powertochange.org/projects/projects_count_hash?#{params.to_query}")
+      http = Net::HTTP.new url.host, url.port
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      http.use_ssl = true
+      res = nil
+      http.start do |agent|
+        puts url.path
+        res = agent.get("#{url.path}?#{url.query}")
+      end
+    end
+
+    return YAML::load(res.body)
   end
 end
