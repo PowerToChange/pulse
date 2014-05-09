6559f1099194e9d1d9d592b7b53b4a19840df398
Colby Warkentin <colby.warkentin@p2c.com>
2014-05-09 16:55:46 -0400

    Change help links

--- config/locales/en-CA.rb
+++ config/locales/en-CA.rb
@@ -17,7 +17,7 @@
     :Bible_Study_short_plural => 'DGs',
     :app_name => 'Pulse',
     :slogan => "<img src='/images/conduitbanner.png'></img>",
-    :help_link => "http://getsatisfaction.com/powertochange",
+    :help_link => "https://groups.google.com/a/p2c.com/forum/#!forum/p2c-s-help-desk",
     :help_header => "Help",
     :alternate_phone => 'Cell Phone',
     
